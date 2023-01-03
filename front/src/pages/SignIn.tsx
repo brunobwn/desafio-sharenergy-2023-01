92c0ec79c4cc0871b1036f6b76d83043b6f55c7a
brunobwn <brunoberwian@gmail.com>
2023-01-03 18:24:45 -0300

    add tailwind forms

--- front/src/pages/SignIn.tsx
+++ front/src/pages/SignIn.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import logo from '../assets/logo_color.png';
 
 const SignIn: React.FC = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1>Growdev</h1>
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-900 font-roboto">
+      <div className="container-sm mx-4 w-96 rounded-lg bg-white p-4 shadow-lg">
+        <img src={logo} alt="SharEnergy Logo" />
+        <p className="mt-4 text-center text-lg">Faça login para continuar</p>
+      </div>
     </div>
   );
 };
