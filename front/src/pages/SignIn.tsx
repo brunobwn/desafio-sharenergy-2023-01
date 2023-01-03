f7c44a531318bf482e20de734d40049ad38ad70d
brunobwn <brunoberwian@gmail.com>
2023-01-03 17:56:01 -0300

    configure react

---
+++ front/src/pages/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn: React.FC = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1>Growdev</h1>
+    </div>
+  );
+};
+
+export default SignIn;
