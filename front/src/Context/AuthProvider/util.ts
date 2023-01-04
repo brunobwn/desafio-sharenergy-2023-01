89514db970c94b169da031effbbe162b79e4cb73
brunobwn <brunoberwian@gmail.com>
2023-01-04 13:09:36 -0300

    change to localhost address api

--- front/src/Context/AuthProvider/util.ts
+++ front/src/Context/AuthProvider/util.ts
@@ -2,11 +2,11 @@ import { UserProps } from './types';
 import api from '../../api';
 
 export function setUserLocalStorage(user: UserProps | null) {
-  localStorage.setItem('u2', JSON.stringify(user));
+  localStorage.setItem('uf31sa9s31sg2', JSON.stringify(user));
 }
 
 export function getUserLocalStorage() {
-  const json = localStorage.getItem('u2');
+  const json = localStorage.getItem('uf31sa9s31sg2');
   if (json) {
     const user = JSON.parse(json);
     return user ?? null;
--- front/src/api.ts
+++ front/src/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://reqres.in/api',
+  baseURL: 'http://localhost:3333/',
 });
