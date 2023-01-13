258abd1726a6e7c8cce638790ce3e58e0b56c501
brunobwn <brunoberwian@gmail.com>
2023-01-13 17:59:26 -0300

    remove unused imports

--- front/src/pages/Clients.tsx
+++ front/src/pages/Clients.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { FaSearch } from 'react-icons/fa';
-import { CgSpinner, CgClose } from 'react-icons/cg';
+import { CgSpinner } from 'react-icons/cg';
 import Paginador from '../components/Paginador';
 import { useAuth } from '../Context/AuthProvider/useAuth';
 import { api } from '../api';
 import { AxiosError, AxiosResponse } from 'axios';
 import ClientCard from '../components/ClientCard';
-import Modal from 'react-modal';
 import ClientModal from '../components/ClientModal';
 
 const NUM_REGISTRO_POR_PAGINA = 12;
@@ -53,11 +52,35 @@ const Clients: React.FC = () => {
         }
       })
       .catch((error: AxiosError) => {
-        setError(error.name);
+        if (error.response?.status === 401) auth.logout();
+        setLoading(false);
+        setError(error.message);
       });
   }, []);
 
-  function handleSearch() {}
+  function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    const searchString = e.target.value;
+
+    if (searchString === '') {
+      setClientsFiltered(clients);
+      return;
+    }
+
+    // compare if string2 exists in string1
+    function includesSearchString(string1: string, string2: string) {
+      return string1.toLowerCase().includes(string2.toLowerCase());
+    }
+
+    const filtered = clients.filter(
+      (client) =>
+        includesSearchString(client.nome, searchString) ||
+        includesSearchString(client.email, searchString) ||
+        includesSearchString(client.cpf, searchString)
+    );
+
+    setClientsFiltered(filtered);
+    setPaginaAtual(1);
+  }
 
   function handleClickCard(id: string) {
     const client = clients.find((client) => client._id === id);
@@ -76,38 +99,50 @@ const Clients: React.FC = () => {
   function handleModalClose() {
     if (!modalOpen) return;
     setModalOpen(false);
+    setClientEdit(null);
+  }
+
+  function handlePaginaAnterior() {
+    if (paginaAtual === 1) return;
+    setPaginaAtual(paginaAtual - 1);
+  }
+
+  function handlePaginaSeguinte() {
+    const ultimaPagina = Math.ceil(clientsFiltered.length / NUM_REGISTRO_POR_PAGINA);
+    if (paginaAtual === ultimaPagina) return;
+    setPaginaAtual(paginaAtual + 1);
   }
 
   return (
     <React.Fragment>
-      <div className="min-h-screen pb-8 bg-gray-100">
+      <div className="min-h-screen bg-gray-100 pb-8">
         <Navbar />
-        <main className="container min-h-full px-5 mx-auto mt-4">
+        <main className="container mx-auto mt-4 min-h-full px-5">
           <div className="flex flex-col sm:flex-row">
-            <h1 className="w-full mb-3 text-xl sm:mb-0 md:mb-0 md:w-fit">Lista de clientes</h1>
-            <fieldset className="relative w-full ml-auto mr-4 h-fit md:max-w-xs">
+            <h1 className="mb-3 w-full text-xl sm:mb-0 md:mb-0 md:w-fit">Lista de clientes</h1>
+            <fieldset className="relative ml-auto mr-4 h-fit w-full md:max-w-xs">
               <input
                 type="text"
                 name="searchUser"
-                className="w-full border rounded-lg shadow-sm border-cyan focus:border-cyan focus:ring-1 focus:ring-cyan"
-                placeholder="Pesquisar"
-                onChange={() => handleSearch()}
+                className="w-full rounded-lg border border-cyan shadow-sm focus:border-cyan focus:ring-1 focus:ring-cyan"
+                placeholder="Pesquisar (Nome/E-mail/CPF)"
+                onChange={(e) => handleSearch(e)}
               />
-              <FaSearch className="absolute -translate-y-1/2 right-4 top-1/2" color="#2da9a9" />
+              <FaSearch className="absolute right-4 top-1/2 -translate-y-1/2" color="#2da9a9" />
             </fieldset>
             <button
               type="button"
-              className="px-3 py-2 mt-4 text-white border rounded-lg shadow-sm bg-cyan opacity-90 hover:opacity-100 hover:shadow-lg focus:border-cyan focus:ring-1 focus:ring-cyan md:mt-0"
+              className="mt-4 rounded-lg border bg-cyan px-3 py-2 text-white opacity-90 shadow-sm hover:opacity-100 hover:shadow-lg focus:border-cyan focus:ring-1 focus:ring-cyan md:mt-0"
               onClick={handleClickCadastro}>
               Cadastrar novo
             </button>
           </div>
           {loading ? (
-            <div className="grid p-8 mt-4 place-items-center md:mt-12">
-              <CgSpinner className="w-12 h-12 animate-spin" color="#2da9a9" />
+            <div className="mt-4 grid place-items-center p-8 md:mt-12">
+              <CgSpinner className="h-12 w-12 animate-spin" color="#2da9a9" />
             </div>
           ) : (
-            <section className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2 sm:gap-2 lg:grid-cols-3 xl:grid-cols-4">
+            <section className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-2 lg:grid-cols-3 xl:grid-cols-4">
               {pageItems.map((client) => (
                 <ClientCard
                   key={client._id}
@@ -121,13 +156,13 @@ const Clients: React.FC = () => {
                 paginaAtual={paginaAtual}
                 inicioIndex={inicioIndex}
                 fimIndex={fimIndex}
-                handlePaginaAnterior={() => {}}
-                handlePaginaSeguinte={() => {}}
+                handlePaginaAnterior={handlePaginaAnterior}
+                handlePaginaSeguinte={handlePaginaSeguinte}
               />
             </section>
           )}
           {error !== '' && (
-            <div className="mt-4 text-sm text-center text-red-800">
+            <div className="mt-4 text-center text-sm text-red-800">
               <p>Erro de servidor, tente novamente</p>
               <p>{error}</p>
             </div>
