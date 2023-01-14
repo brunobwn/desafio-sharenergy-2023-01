02c8b5218e3d5c52cc5aef14e7517b3d88047645
brunobwn <brunoberwian@gmail.com>
2023-01-13 22:02:14 -0300

    add .env variables and example

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+console.log(process.env.REACT_APP_BACKEND_API);
+const backend = process.env.REACT_APP_BACKEND_API ?? 'http://localhost:3333';
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: encodeURI(backend),
 });
 
 export const randomUserApi = axios.create({
