e7d04ed2ec3b1a1adc93ec2705e0cd9b331373e5
brunobwn <brunoberwian@gmail.com>
2023-01-14 22:33:23 -0300

    remove console.log

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-console.log(process.env.REACT_APP_BACKEND_API);
-const backend = process.env.REACT_APP_BACKEND_API ?? 'http://localhost:3333';
+const backend = process.env.REACT_APP_BACKEND_API ?? 'http://localhost:3333/';
+
 export const api = axios.create({
   baseURL: encodeURI(backend),
 });
